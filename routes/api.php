d83e18321607b02bfaa549f44dae90f9af0f762f
Rudi Hardianto <rud.hardianto@gmail.com>
2024-10-25 08:30:26 +0700

    add model, migration, factory, seeder for category & product

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', fn() => response()->json(['message' => 'Hello']));
+Route::apiResource('products', ProductController::class);
